d5435cefe1c513f06e0796ca11b39fb73a16c10b
BuildTools <unconfigured@null.spigotmc.org>
2023-07-30 16:38:23 +0800

    初始提交插件

---
+++ src/main/java/com/vincentx0905/geyserfloodgateusernametonick/GeyserFloodgate_UsernameToNick.java
@@ -0,0 +1,51 @@
+package com.vincentx0905.geyserfloodgateusernametonick;
+
+import com.vincentx0905.geyserfloodgateusernametonick.event.OnPlayerJoin;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class GeyserFloodgate_UsernameToNick extends JavaPlugin implements Listener {
+
+    private final File configFile = new File(getDataFolder(), "config.yml");
+
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = getServer().getPluginManager();
+        Plugin geyserfloodgateplugin = pluginManager.getPlugin("floodgate");
+        if (geyserfloodgateplugin != null) {
+            if (geyserfloodgateplugin.isEnabled()) {
+                getLogger().info("Found Geyser-floodgate plugin, Enable plugin!");
+                pluginManager.registerEvents(this, this);
+                getDataFolder().mkdirs();
+                if (!configFile.exists()) saveDefaultConfig();
+                else {
+                    String configVersion = "V1.0-Alpha";
+                    FileConfiguration config = getConfig();
+                    if (!configVersion.equals(config.getString("ConfigVersion"))) {
+                        saveDefaultConfig();
+                        reloadConfig();
+                        getServer().getLogger().info("Geyser-UsernameToNick plugin update config file to" + configVersion);
+                    }
+                }
+                OnPlayerJoin onPlayerJoin = new OnPlayerJoin(this);
+                getServer().getPluginManager().registerEvents(onPlayerJoin, this);
+                getLogger().info("Geyser-UsernameToNick plugin now Enable!" + " By:VincentX0905(炸蝦)");
+            }
+        } else {
+            getLogger().warning("Not Found Geyser-floodgate plugin, Disable plugin!");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Geyser-UsernameToNick plugin now Disable!" + " By:VincentX0905(炸蝦)");
+    }
+}
---
+++ src/main/java/com/vincentx0905/geyserfloodgateusernametonick/event/OnPlayerJoin.java
@@ -0,0 +1,32 @@
+package com.vincentx0905.geyserfloodgateusernametonick.event;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.geysermc.floodgate.api.FloodgateApi;
+
+import java.util.Objects;
+
+public class OnPlayerJoin implements Listener {
+    private final JavaPlugin plugin;
+
+    public OnPlayerJoin(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        FileConfiguration configuration = plugin.getConfig();
+        if (configuration.getBoolean("Config.Always")){
+            if (FloodgateApi.getInstance().isFloodgatePlayer(event.getPlayer().getUniqueId())  && !configuration.getStringList("Config.Bypass-Player").contains(event.getPlayer().getName())) event.getPlayer().setDisplayName(String.format(Objects.requireNonNull(configuration.getString("Config.Nickname")), FloodgateApi.getInstance().getPlayer(event.getPlayer().getUniqueId()).getUsername()));
+            if (!event.getPlayer().getScoreboardTags().contains("Bedrock")) event.getPlayer().addScoreboardTag("Bedrock");
+        } else if (!event.getPlayer().getScoreboardTags().contains("Bedrock"))
+            if (FloodgateApi.getInstance().isFloodgatePlayer(event.getPlayer().getUniqueId()) && !configuration.getStringList("Config.Bypass-Player").contains(event.getPlayer().getName()))
+                if (!event.getPlayer().getScoreboardTags().contains("Bedrock")) {
+                    event.getPlayer().setDisplayName(String.format(Objects.requireNonNull(configuration.getString("Config.Nickname")), FloodgateApi.getInstance().getPlayer(event.getPlayer().getUniqueId()).getUsername()));
+                    event.getPlayer().addScoreboardTag("Bedrock");
+                }
+    }
+}
